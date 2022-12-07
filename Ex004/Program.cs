81e528485f584a555271b25e8d11421a3bd9d4b6
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-07 19:49:51 +0300

    Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int a;
+int n = 2;
+
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+
+while (n < a)
+{
+   if (n % 2 == 0)
+   {
+      Console.Write($"{n} ");
+   }
+   else 
+   {
+      Console.Write($" ");
+   }
+   n = n + 1;
+}
