59789b4cc9351a2b32464e198fea9a625fc913c1
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-07 16:56:10 +0300

    Ex001

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,17 +1,1 @@
-﻿string user1;
-string user2;
-int max;
-int min;
-
-Console.Write("Введите первое число:");
-user1 = Console.Readline()!;
-
-Console.Write("Введите второе число:");
-user2 = Console.Readline()!;
-
-if (user1 > user2) max = user1; min = user2;
-if (user1 < user2) max = user2; min = user1;
-
-
-Console.WriteLine($"Максимальное значение = {max}");
-Console.WriteLine($"Минимальное значение = {min}");
+﻿// Hello!
