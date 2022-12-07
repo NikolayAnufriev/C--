d37dc66741775afcf59d099c4245452c702bc63b
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-08 01:47:47 +0300

    Ex006

---
+++ Ex006/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+int a;
+int size;
+
+Console.Write($"Введите число: ");
+int.TryParse(Console.ReadLine()!, out a);
+string number = a.ToString();
+size = number.Length;
+
+if (size < 3)
+    Console.Write($"Третьей цифры нет");
+else
+    Console.Write($"Цифра = {number[2]}");
