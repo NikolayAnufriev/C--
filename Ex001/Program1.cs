086446fc303f1e49bfc438858beb3bf7de2d07e2
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-07 16:51:27 +0300

    Ex001

---
+++ Ex001/Program1.cs
@@ -0,0 +1,24 @@
+﻿//  Первое домашнее задание
+//  Задача 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int a = 0;
+int b = a;
+
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+if (a > b)
+{
+    Console.Write($"Максимальное значение = {a}\n");
+    Console.Write($"Минимальное значение = {b}");
+}
+else
+{
+    Console.Write($"Максимальное значение = {b}\n");
+    Console.Write($"Минимальное значение = {a}");
+}
+
+
+
--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello!");
+﻿string user1;
+string user2;
+int max;
+int min;
+
+Console.Write("Введите первое число:");
+user1 = Console.Readline()!;
+
+Console.Write("Введите второе число:");
+user2 = Console.Readline()!;
+
+if (user1 > user2) max = user1; min = user2;
+if (user1 < user2) max = user2; min = user1;
+
+
+Console.WriteLine($"Максимальное значение = {max}");
+Console.WriteLine($"Минимальное значение = {min}");
