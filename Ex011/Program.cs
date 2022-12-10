4052eb86d0621e1f9ff5b87aee7ddf1f404ad18f
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-11 01:16:04 +0300

    Ex011

---
+++ Ex011/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int a;
+int b;
+double c;
+
+Console.Write($"Введите первое число 'a': ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число 'b': ");
+int.TryParse(Console.ReadLine()!, out b);
+
+Console.Write($"Ответ: {c = Math.Pow(a,b)}");
