897d6ed825253ed8cda67f2e101452a2289afc60
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-11 02:24:55 +0300

    Ex013

---
+++ Ex012/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.  string number = a.ToString();
+
+int i = 0;
+int a = 0;
+int sum = 0;
+
+Console.Write("Введите ваше число ");
+int.TryParse(Console.ReadLine()!, out a);
+string array = a.ToString();
+int size = array.Length;
+
+while (i < size)
+{
+sum = array[i] + a;
+i = i + 1;
+}
+Console.Write($"Ответ: {sum}");
