f3726c78c3777517504440a7d0069ec4716e0d97
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-11 23:41:06 +0300

    Ex013

--- Ex013/Program.cs
+++ Ex013/Program.cs
@@ -1,35 +1,46 @@
-﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-// int array = new Random().Next(0,20);
-// Console.Write($"Итоговый массив = {array}\n");
+ --Можно сделать как показывали в лекции (если убрать слеши можно запустить программу)--
 
+ int[] array = new int[8];
+
+ void Newarray(int[] array)
+ {
+     int size = array.Length;
+     int i  = 0;
+     while (i < size)
+     {
+         array[i] = new Random().Next(0, 11); 
+         i++;
+     }
+ }
+
+ void PrintArray(int[] arrayprint)
+
+ {
+     int sizearray = arrayprint.Length;
+     int index = 0;
+     while (index < sizearray)
+     {
+         Console.Write($"{arrayprint[index]} ");
+         index++;
+     }
+ }
+
+
+ Newarray(array);
+ PrintArray(array);
+
+Но я думаю что проще это делать так как показано ниже*/ 
 
 int[] array = new int[8];
 
-void Newarray(int[] array)
+int i = 0;
+int size = array.Length;
+
+while (i < size)
 {
-    int size = array.Length;
-    int i  = 0;
-    while (i < size)
-    {
-        array[i] = new Random().Next(0, 11); 
-        i++;
-    }
+  array[i] = new Random().Next(0, 11); 
+  Console.Write($"{array[i]} ");
+  i++;
 }
-
-void PrintArray(int[] arrayprint)
-
-{
-    int sizearray = arrayprint.Length;
-    int index = 0;
-    while (index < sizearray)
-    {
-        Console.Write($"{arrayprint[index]} ");
-        index++;
-    }
-}
-
-
-
-Newarray(array);
-PrintArray(array);
