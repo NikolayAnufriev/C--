7afc199ba2c959557f22718569c37e8f4dd01da2
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-12 01:04:55 +0300

    Ex013

--- Ex012/Program.cs
+++ Ex012/Program.cs
@@ -1,16 +1,29 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.  string number = a.ToString();
 
+// double a = 0;
+// double b = 0;
 
-double a = 0;
-double b = 0;
+// Console.Write("Введите ваше число ");
+// double.TryParse(Console.ReadLine()!, out a);
 
-Console.Write("Введите ваше число ");
-double.TryParse(Console.ReadLine()!, out a);
+// if (a < 10)
+//    Console.Write($"Ответ: {a}");
+// else 
+//    a = (a / 10); 
+//    b = ((a % 10) * 10);
 
-if (a < 10)
-   Console.Write($"Ответ: {a}");
-else 
-   a = (a / 10); 
-   b = ((a % 10) * 10);
+//    Console.Write($"Ответ: {a}");
 
-   Console.Write($"Ответ: {a}");
+
+int i = 0;
+int sum = 0;
+
+Console.Write($"Введите число: ");
+string array = (Console.ReadLine()!);
+int size = array.Length;
+
+while (i < size)
+  sum = sum + array[i];
+  i++;
+
+Console.Write($"Сумма = {sum}");
