2898a4068869f87684ec47ae2bd5f63400f8de07
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-12 22:16:22 +0300

    Ex013

--- Ex013/Program.cs
+++ Ex013/Program.cs
@@ -1,37 +1,4 @@
-﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
- --Можно сделать как показывали в лекции (если убрать слеши можно запустить программу)--
-
- int[] array = new int[8];
-
- void Newarray(int[] array)
- {
-     int size = array.Length;
-     int i  = 0;
-     while (i < size)
-     {
-         array[i] = new Random().Next(0, 11); 
-         i++;
-     }
- }
-
- void PrintArray(int[] arrayprint)
-
- {
-     int sizearray = arrayprint.Length;
-     int index = 0;
-     while (index < sizearray)
-     {
-         Console.Write($"{arrayprint[index]} ");
-         index++;
-     }
- }
-
-
- Newarray(array);
- PrintArray(array);
-
-Немного модифицировав код можно делать так как показано ниже*/ 
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. */ 
 
 int[] array = new int[8];
 
