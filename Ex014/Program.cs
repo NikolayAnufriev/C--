21be3f994de413abb99b9bcf95f433b5dd1da54f
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-13 17:54:29 +0300

    Ex014

---
+++ Ex014/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 34:
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+*/
+
+int n;
+
+Console.WriteLine("Введите размер массива ");
+int.TryParse(Console.ReadLine()!, out n);
+
+int[] array = new int[n];
+
+int i = 0;
+int j = 0;
+int count = 0;
+int size = array.Length;
+
+while (i < size)
+{
+  array[i] = new Random().Next(100, 1000); 
+  Console.Write($"{array[i]} ");
+  i++;
+}
+while (j < size)
+{
+  if  (array[j] % 2 == 0) { j++; count = count + 1; }   
+
+  else { j++; }     
+} 
+Console.Write($" \n");
+Console.Write($"Количество чётных чисел в массиве = {count} ");
