eca110d5123638b6a6f4e0099c00a259f2334f68
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-13 18:07:00 +0300

    Ex015

---
+++ Ex015/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях. */
+
+int n;
+
+Console.WriteLine("Введите размер массива ");
+int.TryParse(Console.ReadLine()!, out n);
+
+int[] array = new int[n];
+
+int i = 0;
+int j = 0;
+int sum = 0;
+int size = array.Length;
+
+Console.Write($"Заданный массив ->  ");
+while (i < size)
+{
+  array[i] = new Random().Next(0, 11); 
+  Console.Write($"{array[i]} ");
+  i++;
+}
+while (j < size)
+{
+  sum += array[j];
+  j = j +2;
+} 
+Console.Write($" \n");
+Console.Write($"Сумма элементов, стоящих на нечётных позициях. = {sum} ");
