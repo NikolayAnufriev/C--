d47e303636b88df187dad5ea060643d18de2d023
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-13 21:52:16 +0300

    Ex018

---
+++ Ex018/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями:
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем. */
+
+double k1; double k2;
+double b1; double b2;
+
+Console.Write("введите значение b1 ");
+double.TryParse(Console.ReadLine()!, out b1);
+Console.Write("введите значение b2 ");
+double.TryParse(Console.ReadLine()!, out b2);
+Console.Write("введите значение k1 ");
+double.TryParse(Console.ReadLine()!, out k1);
+Console.Write("введите значение k2 ");
+double.TryParse(Console.ReadLine()!, out k2);
+
+double x = (b2 - b1) / (k1 - k2);
+double y =  k1 * x + b1;
+
+Console.Write($"Координаты пересечения:\n x = {x} \n y = {y}");
