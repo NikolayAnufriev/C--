a2d5905c07a5e0faff2e623296fe77a443ed358c
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-17 17:07:34 +0300

    Ex019

--- Ex019/Program.cs
+++ Ex019/Program.cs
@@ -9,7 +9,7 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(100) / 10.0;
+        array[i, j] = new Random().Next(-100, 101) / 10.0;
         Console.Write($"{array[i, j]} ");
     }
     Console.WriteLine();
