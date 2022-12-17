7f810ea442688b1f748f7fe7e7225a65529af4b5
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-17 19:34:10 +0300

    Ex021

---
+++ Ex021/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое 
+элементов в каждом столбце. */
+
+int[,] array = new int[4, 4];
+
+Console.WriteLine("Заданный двумерный массив: \n");
+
+for (int i = 0; i < array.GetLength(0); i++) // Создание массива
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(11);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+double sum = 0;
+
+for (int j = 0; j < array.GetLength(1); j++) // Вычесление среднего
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i,j];
+    }
+    Console.Write($"Среднее {j + 1} столбца = {Math.Round(sum / array.GetLength(0), 1)}  \n");
+}
