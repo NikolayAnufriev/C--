4c82b462b877e575cba30ba4634ccd6b08991dff
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-17 16:59:12 +0300

    Ex019

---
+++ Ex019/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 47. Задайте двумерный массив размером mxn, заполненный случайными вещественными числами 
+m = 3, n =4 */
+
+double[,] array = new double[3, 4];
+
+Console.WriteLine("Заданный двумерный массив: \n");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(100) / 10.0;
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
