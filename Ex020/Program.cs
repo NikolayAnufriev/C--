1d3398b6f6a9a597bd6ca4b339530e1d1e3cb3a1
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-17 18:45:30 +0300

    Ex020

--- Ex020/Program.cs
+++ Ex020/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет. */
+
+double[,] array = new double[3, 3];
+
+Console.WriteLine("Заданный двумерный массив: \n");
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-100, 101) / 10.0;
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+int ix; int iy;
+Console.Write($"Введите индекс строки: ");
+int.TryParse(Console.ReadLine(), out ix);
+Console.Write($"Введити индекс столбца: ");
+int.TryParse(Console.ReadLine(), out iy);
+
+if (ix >= 0 && ix < array.GetLength(0) && iy >= 0 && iy < array.GetLength(1))
+   Console.Write($"Ваше число -> {array[ix, iy]} ");
+else 
+   Console.Write("*Ошибка* Индекс должен быть больше 0 и не больше размера массива! ");
