86f612f2089d880343a79cb3ab95e5373c8b3f8b
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-18 11:52:56 +0300

    Ex021

--- Ex021/Program.cs
+++ Ex021/Program.cs
@@ -18,7 +18,7 @@ Console.WriteLine();
 
 double sum = 0;
 
-for (int j = 0; j < array.GetLength(1); j++) // Вычесление среднего
+for (int j = 0; j < array.GetLength(1); j++) // Вычисление среднего
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
