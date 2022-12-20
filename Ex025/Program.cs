b2a4e2496a5fa4dd2ff73c69e6fa8c9829679729
NikolayAnufriev <nikolanius132@gmail.com>
2022-12-20 08:45:39 +0300

    Ex026

--- Ex025/Program.cs
+++ Ex025/Program.cs
@@ -1,3 +1,20 @@
 ﻿/* Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента. */
 
+int[,,] array = new int[2,2,2];
+
+Console.WriteLine("Заданный трехмерный массив массив: \n");
+int number = 10;
+for (int x = 0; x < array.GetLength(0); x++)
+{
+    for (int y = 0; y < array.GetLength(1); y++)
+    {
+        for (int z = 0; z < array.GetLength(2); z++)
+        {
+            array[x, y, z] = new Random().Next(10, 50) + number;
+            number++;
+            Console.Write($"{array[x, y, z]} ({x},{y},{z}) ");
+        }
+        Console.WriteLine();
+    }    
+}
