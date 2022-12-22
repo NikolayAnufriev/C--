372820e8bf1229274d7204a96939833eb00efc89
Nikolai Anufriev <nikolanius13@gmail.com>
2022-12-22 18:10:56 +0300

    Ex027

---
+++ Ex027/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии. */
+
+int n;
+
+Console.Write($"Введите число:  ");
+int.TryParse(Console.ReadLine()!, out n);
+
+string array(int a, int b)
+{
+    if (a <= b)
+       return a.ToString();
+    return $"{a } {array(a - 1, b)}";
+}
+
+Console.Write($"{array(n, 1)}");
